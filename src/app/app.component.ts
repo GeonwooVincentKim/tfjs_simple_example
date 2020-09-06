283b1fedded33c0111b7ac50231c12259e3ecbe0
kdsnop@gmail.com <kdsnop@gmail.com>
2020-09-06 20:18:20 +0900

    Generated 'app.component.ts'.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,7 @@ export class AppComponent implements OnInit {
   // training some randomly generated static data.
   prediction: any;  
 
-  model: tf.ModelTensorInfo;
+  model: tf.ModelCompileArgs;
   predictions: any;
 
   @ViewChild(DrawableDirective) canvas;
