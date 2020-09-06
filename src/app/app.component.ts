b9fe7d8f7989bf399c3d4318dcf6680e7a2c86b3
kdsnop@gmail.com <kdsnop@gmail.com>
2020-09-06 14:21:00 +0900

    Created tensor-one-dimensional lists.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,5 +29,12 @@ export class AppComponent implements OnInit {
   
     // Prepare the model for training: Specify the loss and the optimizer.
     this.linearModel.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+    
+    // Training data, completely random stuff.
+    // xs -> training features for the Data-Set.
+    // ys -> The label for 'xs'-DataSet data.
+    const xs = tf.tensor1d([3.2, 4.4, 5.5, 6.71, 6.98, 7.168, 9.779, 6.182, 7.59, 2.16, 7.04]); // --> One-Dimensional
+    const ys = tf.tensor1d([1.6, 2.7, 2.9, 3.19, 1.684, 2.53, 3.366, 2.596, 2.53, 1.22, 2.81]);
+
   }
 }
