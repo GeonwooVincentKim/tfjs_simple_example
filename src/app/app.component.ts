50510d6f273c8169359e28e2aa1b3ecff54cdb74
kdsnop@gmail.com <kdsnop@gmail.com>
2020-09-06 13:50:44 +0900

    Before createing new '.md' file.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,6 +29,17 @@ export class AppComponent implements OnInit {
     2. Adding a Dense-Layer or a fully connected Layer
     that will output a shape of one and also take 
     an input with a shape one. 
+
+    3. Define the loss metric which in this case,
+    will be 'meanSquaredError', and minimize the 'meanSquaredError' of the predictions
+    from the algorithm.
+
+    4. Define an optimizer which is the function that
+    determines how we reach this actual Minimum-Error-Point.
+
+    5. SGD statnd sfor stochastic-gradient-descent,
+    and there's a number of different optimizers that you can
+    choose from.
   */
   async trainNewModel(){
     // Define a Model for Linear-Regression.
